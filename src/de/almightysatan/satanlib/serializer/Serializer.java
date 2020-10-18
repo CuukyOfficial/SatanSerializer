6bc31f5186dfca476959e192e381f5df6b8c884a
Almighty-Satan <43804777+Almighty-Satan@users.noreply.github.com>
2020-10-18 12:42:35 +0200

    changed version to 1.2.1

--- src/de/almightysatan/satanlib/serializer/Serializer.java
+++ src/de/almightysatan/satanlib/serializer/Serializer.java
@@ -11,7 +11,7 @@ import java.util.concurrent.CopyOnWriteArrayList;
 
 public class Serializer {
 	
-	public static final String VERSION = "1.2.0";
+	public static final String VERSION = "1.2.1";
 	public static final int ENCODER_VERSION = 1;
 	
 	static final byte INDEX_BREAK = -1;
